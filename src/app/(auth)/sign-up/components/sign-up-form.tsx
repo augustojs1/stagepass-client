26ccaca25d318b3f2a11e557b2e635cdf1f14428
augustojs1 <augustojsouza1@gmail.com>
2025-10-09 20:33:47 -0300

    feat: create sign up form

--- src/app/(auth)/sign-up/components/sign-up-form.tsx
+++ src/app/(auth)/sign-up/components/sign-up-form.tsx
@@ -1,34 +1,42 @@
 "use client";
 
-import { Input } from "@/app/components";
-import { Button } from "@/app/components/ui/form/button";
 import Link from "next/link";
 
-export function LoginForm() {
+import { Input } from "@/app/components";
+import { Button } from "@/app/components/ui/form/button";
+
+export function SignUpForm() {
   return (
     <div className="max-w-[551px] w-full bg-white rounded-md py-9 px-8">
       <div className="mb-10">
         <h1 className="font-display font-bold text-black-3 text-[48px] text-center">
-          Welcome back.
+          Welcome.
         </h1>
         <p className="font-display text-[18px] font-500 text-gray-4 text-center">
-          Sign in to continue
+          Create an account
         </p>
       </div>
       <form className="max-w-[359px] w-full mx-auto flex flex-col gap-7">
+        <Input label="First Name" placeholder="Joe" type="text" />
+        <Input label="Last Name" placeholder="Doe" type="text" />
         <Input label="Email" placeholder="email@email.com" type="text" />
         <Input label="Password" placeholder="*********" type="password" />
+        <Input
+          label="Confirm Password"
+          placeholder="*********"
+          type="password"
+        />
         <Button className="mt-2" variant="primary" block>
-          Login
+          Sign Up
         </Button>
       </form>
       <p className="text-center mt-8 text-[14px] font-[400] text-black-3">
-        Don&apos;t have an account?
+        Already have an account?
         <Link
-          href="/sign-up"
+          href="/login"
           className="text-primary text-[14px] font-[700] ml-1"
         >
-          Sign up
+          Login
         </Link>
       </p>
     </div>
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,16 @@
+import { Metadata } from "next";
+
+import { SignUpForm } from "./components";
+
+export const metadata: Metadata = {
+  title: "StagePass | Sign Up",
+  description: "Sign Up",
+};
+
 export default function SignUpPage() {
   return (
-    <section>
-      <h1>Sign Up</h1>
+    <section className="h-screen w-full flex justify-center items-center px-5">
+      <SignUpForm />
     </section>
   );
 }
