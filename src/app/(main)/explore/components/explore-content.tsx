405ad1356be04ca932a2a6ef0845e44d01e740bd
augustojs1 <augustojsouza1@gmail.com>
2025-10-14 21:24:29 -0300

    fix: show filter logic

--- src/app/(main)/explore/components/explore-content.tsx
+++ src/app/(main)/explore/components/explore-content.tsx
@@ -8,38 +8,7 @@ import { SearchInput } from "@/app/components";
 import { ExploreEventCard } from "./explore-event-card";
 
 export function ExploreContent() {
-  const [isMobile, setIsMobile] = React.useState<boolean>(false);
   const [showFilter, setShowFilter] = React.useState<boolean>(false);
-
-  function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-
-    return {
-      width,
-      height,
-    };
-  }
-
-  React.useEffect(() => {
-    function handleResize() {
-      const size = getWindowDimensions();
-
-      if (size.width >= 767) {
-        setIsMobile(true);
-      }
-    }
-
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  function handleShowFilter() {
-    setShowFilter(!showFilter);
-    setIsMobile(!isMobile);
-  }
 
   return (
     <section className="mb-20">
@@ -47,17 +16,18 @@ export function ExploreContent() {
         <SearchInput />
         <button
           className="flex md:hidden justify-center items-center border border-primary rounded-[6px] w-12 h-12"
-          onClick={handleShowFilter}
+          onClick={() => setShowFilter(!showFilter)}
         >
           <ListFilter size={18} color="#636ae8" />
         </button>
       </div>
       <div className="grid grid-cols-1 md:[grid-template-columns:minmax(0,276px)_minmax(0,856px)] gap-4 justify-center">
-        {isMobile || showFilter ? (
-          <aside>
-            <EventsFilter />
-          </aside>
-        ) : null}
+        <aside className="sm:flex hidden">
+          <EventsFilter />
+        </aside>
+        <div className="flex sm:hidden">
+          {showFilter ? <EventsFilter /> : null}
+        </div>
         <div className="flex flex-col gap-4">
           <div className="py-4">
             <p className="text-[1rem] text-black-3">
