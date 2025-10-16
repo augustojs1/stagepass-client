97bf1e70854ff84d16c0e1460ea96d07a6db5b70
augustojs1 <augustojsouza1@gmail.com>
2025-10-16 19:50:46 -0300

    feat: create new input variant

--- src/app/components/ui/form/input.tsx
+++ src/app/components/ui/form/input.tsx
@@ -1,24 +1,44 @@
+"use client";
+
 import React from "react";
+
+import { cn } from "@/lib/utils";
 
 interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  variant?: "underlined" | "outlined";
 }
 
-export const Input: React.FC<InputFieldProps> = ({ label, id, ...props }) => {
+export const Input: React.FC<InputFieldProps> = ({
+  label,
+  id,
+  variant = "outlined",
+  className,
+  ...props
+}) => {
   const inputId = id || label.toLowerCase().replace(/\s+/g, "-");
+
+  const base = "font-body placeholder-gray-1 text-[14px] px-2 py-1";
+
+  const variants = {
+    underlined:
+      "border-b border-gray-1 focus:border-primary focus:outline-none text-gray-2",
+    outlined:
+      "bg-white-3 rounded-[6px] focus:border-1 focus:border-primary focus:outline-none text-gray-2",
+  };
 
   return (
     <div className="flex flex-col w-full">
       <label
         htmlFor={inputId}
-        className="font-body text-[14px] font-[500] text-black-3 mb-1"
+        className="font-body text-[14px] font-[500] text-black-2"
       >
         {label}
       </label>
       <input
         id={inputId}
         {...props}
-        className="font-body border-b  border-gray-1 focus:border-primary focus:outline-none py-1 placeholder-gray-1 text-[14px] text-gray-2"
+        className={cn(base, variants[variant], className)}
       />
     </div>
   );
