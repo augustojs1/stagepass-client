05b652dcbb233659b2d699bd1515a546a985f747
augustojs1 <augustojsouza1@gmail.com>
2026-01-08 20:28:31 -0300

    feat: create disabled style for input component

--- src/components/ui/form/input.tsx
+++ src/components/ui/form/input.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-
 import { cn } from "@/lib/utils";
 
 interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -16,46 +15,63 @@ export const Input: React.FC<InputFieldProps> = ({
   variant = "outlined",
   className,
   error,
+  disabled,
   ...props
 }) => {
   const inputId = id || label.toLowerCase().replace(/\s+/g, "-");
 
   const base =
-    "font-body placeholder-gray-1 text-[14px] px-2 py-2 transition-colors duration-150 ease-in-out";
+    "font-body text-[14px] px-2 py-2 transition-colors duration-150 ease-in-out";
 
   const variants = {
     underlined:
       "border-b border-gray-1 focus:border-primary focus:outline-none text-gray-2",
     outlined:
-      "bg-white-3 rounded-[6px] focus:border-1 focus:border-primary focus:outline-none text-gray-2",
+      "bg-white-3 rounded-[6px] border border-transparent focus:border-primary focus:outline-none text-gray-2",
   };
 
   const errorVariant =
     variant === "underlined"
-      ? "border-b border-red-2 focus:border-red-2 text-red-2 focus:outline-none"
-      : "focus:border-1 focus:border-red-2 text-red-2 bg-white-3 rounded-[6px] focus:outline-none";
+      ? "border-b border-red-2 text-red-2 focus:border-red-2"
+      : "border border-red-2 text-red-2 bg-white-3";
+
+  const disabledVariant =
+    variant === "underlined"
+      ? "border-b border-gray-200 text-gray-400 cursor-not-allowed bg-transparent"
+      : "bg-gray-100 border border-gray-200 text-gray-400 cursor-not-allowed rounded-[6px]";
 
   return (
     <div className="flex flex-col w-full">
       <label
         htmlFor={inputId}
-        className={`font-body text-[14px] font-[500] ${
-          error ? "text-red-2" : "text-black-2"
-        }`}
+        className={cn(
+          "font-body text-[14px] font-[500]",
+          disabled
+            ? "text-gray-400 cursor-not-allowed"
+            : error
+            ? "text-red-2"
+            : "text-black-2"
+        )}
       >
         {label}
       </label>
+
       <input
         id={inputId}
+        disabled={disabled}
+        aria-disabled={disabled}
         {...props}
         className={cn(
           base,
-          error ? errorVariant : variants[variant],
+          disabled ? disabledVariant : error ? errorVariant : variants[variant],
           className
         )}
       />
+
       <div className="h-[14px] mt-1">
-        {error && <p className="text-red-2 text-[12px] font-body">{error}</p>}
+        {!disabled && error && (
+          <p className="text-red-2 text-[12px] font-body">{error}</p>
+        )}
       </div>
     </div>
   );
